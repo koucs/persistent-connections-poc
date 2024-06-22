84f426b528fdd8584e397111031f7f4248cb2ca4
koucs <kou.cshu@gmail.com>
2024-06-23 00:08:23 +0900

    update readme

--- sse/server.js
+++ sse/server.js
@@ -13,10 +13,13 @@ app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  // ヘッダーをすぐにクライアントに送信
+  // その後のデータの送信を待つことなくクライアントがヘッダーを解析できるようになる
   res.flushHeaders();
 
   clients.push(res);
 
+  // 接続が閉じられたクライアントはclients配列から削除され、その後のイベントの送信対象から除外
   req.on('close', () => {
     clients = clients.filter(client => client !== res);
   });
